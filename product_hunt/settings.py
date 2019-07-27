c78128b8c0991420595086e71ff7d91cc59781ea
骑着毛驴去旅行 <1379613157@qq.com>
2019-07-27 14:20:56 +0800

    标题栏设置完成
    标题栏logo使用static静态文件
    要在主文件setting.py中添加static的url路径
    STATIC_URL = '/static/'
    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
    
    STATIC_DIRS = [
        os.path.join(BASE_DIR, 'products/static')
    ]

--- product_hunt/settings.py
+++ product_hunt/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'products/static')
+]
