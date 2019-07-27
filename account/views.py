b26226c7b5d8bae5d21968f52fa9e5c4eec545aa
骑着毛驴去旅行 <1379613157@qq.com>
2019-07-27 16:57:57 +0800

    添加了注册的页面

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+def sign_up(request):
+    if request.method == 'GET':
+        return render(request, 'sign_up.html')
+    elif request.method == 'POST':
+        user_name = request.POST['用户名']
+        password1 = request.POST['密码']
+        password2 = request.POST['确认密码']
+        try:
+            User.objects.get(username = user_name)
+            return render(request, 'sign_up.html',{'用户名错误':"该用户名已存在"})
+        except User.DoesNotExist:
+            if password1 == password2:
+                User.objects.create_user(username=user_name, password= password1)
+                return redirect('主页')
+            else:
+                return render(request, 'sign_up.html', {'密码错误': "两次输入的密码不一致"})
+
--- product_hunt/urls.py
+++ product_hunt/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import products.views
+import account.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', products.views.product_list),
+    path('', products.views.product_list,name="主页"),
+    path('account/', include('account.urls')),
+
 ]
