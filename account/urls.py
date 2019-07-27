b22ba6c62cdc6de6c42b26ef2732c7c590e3c68f
骑着毛驴去旅行 <1379613157@qq.com>
2019-07-27 22:30:47 +0800

    添加了登录和退出的页面
    T

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sign_up',views.sign_up, name='注册页面' ),
-
+    path('sign_up/',views.sign_up, name='注册页面' ),
+    path('login/',views.login, name='登录页面' ),
+    path('logout/',views.logout, name='退出页面' ),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 # Create your views here.
 
 
@@ -20,3 +21,22 @@ def sign_up(request):
             else:
                 return render(request, 'sign_up.html', {'密码错误': "两次输入的密码不一致"})
 
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        user_name = request.POST['用户名']
+        pass_word = request.POST['密码']
+        user = auth.authenticate(username=user_name,password=pass_word)
+        if user is None:
+            return render(request, 'login.html', {'错误': "用户名或密码错误"})
+        else:
+            auth.login(request,user)
+            return redirect('主页')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('主页')
