2691328c302e2abbbf8dfbff3573529a43fdc9fa
骑着毛驴去旅行 <1379613157@qq.com>
2019-07-27 13:28:47 +0800

    创建了主题模板

--- product_hunt/settings.py
+++ product_hunt/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #注册APP
+    'products',
+    'account',
 ]
 
 MIDDLEWARE = [
